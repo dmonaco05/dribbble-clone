f949933e93ef76f4d62417fbc562d09621f8ef10
Dominic Monaco <dmonaco05@hotmail.com>
2015-04-15 11:28:16 -0400

    add comments to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
+  has_many :comments
   has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
